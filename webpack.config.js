d4639d139036b68f8a5e3d77a46d45dd103de134
Miyazaki <ts-gota.a.miyazaki@p49154.intra.rakuten.co.jp>
2019-03-01 11:11:39 +0900

    umd

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "stopwatch.js",
-    library: "stopwatch"
+    library: "stopwatch",
+    libraryTarget: "umd"
   }
 };
