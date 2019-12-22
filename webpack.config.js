6aa28b1af2c066de3ac48b728792324f818d89ae
mya-zaki <gota.miyazaki@gmail.com>
2019-12-23 00:23:18 +0900

    modify package settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
